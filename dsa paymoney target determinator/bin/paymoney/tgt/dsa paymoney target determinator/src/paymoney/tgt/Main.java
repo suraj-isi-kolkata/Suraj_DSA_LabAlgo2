c88a2ae54a72ec6a10784d9fd38a6b5aa679478d
suraj-isi-kolkata <61353835+suraj-isi-kolkata@users.noreply.github.com>
2024-02-09 01:32:52 +0530

    Add files via upload

---
+++ dsa paymoney target determinator/bin/paymoney/tgt/dsa paymoney target determinator/src/paymoney/tgt/Main.java
@@ -0,0 +1,13 @@
+package paymoney.tgt;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		PayMoneyTargetDeterminator determinator = 
+				new PayMoneyTargetDeterminator();
+		determinator.determine();
+		
+	}
+}
